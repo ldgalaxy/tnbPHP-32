1d5f200a3f1499673e34682ab2157e6ff2ad0405
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-12 08:19:19 -0600

    Added the divide function

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ function add($number1, $number2 ){
    return $result;
 }
 
-echo add(10, 10);
+function divide($number1, $number2){
+   $result = $number1 / $number2;
+   return $result;
+}
 
+$sum = divide(add(10, 10), add(5, 5));
+echo $sum;
 
 ?>
